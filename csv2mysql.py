9728d1b9edbcfcb148eeb36dcad2429ef79f9f73
iamdonnie888 <15321923885@163.com>
2025-03-25 16:30:02 +0800

    添加批量csv2mysql功能

--- csv2mysql.py
+++ csv2mysql.py
@@ -5,12 +5,15 @@ import mysql.connector
 mydb = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="123456",
+    password="benben",
     database="av_numbers_db"
 )
 
-# 读取 CSV 文件
-df = pd.read_csv(r'./AVNB/csv/AVLIST_1.csv')
+start_page = int(input('请输入起始文件号码：'))
+end_page = int(input('请输入终止文件号码：'))
+
+csv_base_path = r'./AVNB/csv/AVLIST_'
+csv_tail_path = '.csv'
 
 # 创建游标
 mycursor = mydb.cursor()
@@ -24,14 +27,19 @@ CREATE TABLE IF NOT EXISTS av_number (
     Content VARCHAR(1023) CHARACTER SET utf8mb4
 )
 """
-
 mycursor.execute(create_table_sql)
 
-# 插入数据
-for index, row in df.iterrows():
-    sql = "INSERT INTO av_number (Number, Name, Content) VALUES (%s, %s, %s)"
-    val = (row['Number'], row['Name'], row['Content'])
-    mycursor.execute(sql, val)
+for page in range(start_page, end_page + 1):
+    page = str(page)
+    csv_path = csv_base_path + page + csv_tail_path
+    # 读取 CSV 文件
+    df = pd.read_csv(csv_path)
+
+    # 插入数据
+    for index, row in df.iterrows():
+        sql = "INSERT INTO av_number (Number, Name, Content) VALUES (%s, %s, %s)"
+        val = (row['Number'], row['Name'], row['Content'])
+        mycursor.execute(sql, val)
 
 # 提交更改
 mydb.commit()
